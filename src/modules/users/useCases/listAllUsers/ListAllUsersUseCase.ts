54f4b619aed17bc70ef9fd9efa9aa7e3bc3bf63e
palomaraujo <palomaraujo2@hotmail.com>
2022-07-27 15:43:53 -0300

    implementação do desafio

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -4,12 +4,19 @@ import { IUsersRepository } from "../../repositories/IUsersRepository";
 interface IRequest {
   user_id: string;
 }
-
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const callUser = this.usersRepository.findById(user_id);
+    if (!callUser) {
+      throw new Error("User not found");
+    }
+    if (callUser.admin === false) {
+      throw new Error("User caller is not an admin user");
+    }
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
